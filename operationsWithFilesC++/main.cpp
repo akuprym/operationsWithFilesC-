df8186fab007353be40624403907884f5febe24c
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-06-30 15:22:47 +0300

    add fout.open()

---
+++ operationsWithFilesC++/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  secondProjectC++
+//
+//  Created by admin on 29.06.24.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+    string path = "myfile.txt";
+    
+    ofstream fout;
+    fout.open(path);
+    
+    if (!fout.is_open()) {
+        cout << "Ошибка открытия файла" << endl;
+    } else {
+        fout << "Это наши данные";
+    }
+    
+    fout.close();
+
+    return 0;
+}
